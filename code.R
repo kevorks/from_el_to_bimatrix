690c223332efa9c9ba085f2580160757248288b2
sevag <s.d.kevork@gmail.com>
2021-05-03 11:31:08 +0200

    Create code.R

---
+++ code.R
@@ -0,0 +1,13 @@
+library(Matrix)
+from_el_to_bimatrix <- function(el, mode1, mode2){
+  bimatrix <- spMatrix(nrow = length(unique(el[,mode1])),
+                       ncol = length(unique(el[,mode2])),
+                       i = as.numeric(factor(el[,mode1])),
+                       j = as.numeric(factor(el[,mode2])),
+                       x = rep(1, length(as.numeric(el[,mode1]))))
+  
+  rownames(bimatrix) <- levels(factor(el[,mode1]))
+  colnames(bimatrix) <- levels(factor(el[,mode2]))
+  bimatrix <- as.matrix(bimatrix)
+  return(bimatrix)
+}
